3b137047877fc3807fd6b86a03d86528e727db3d
Denis Lafontant <icebox827@gmail.com>
2021-05-11 11:07:11 -0400

    Add validations to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :author, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :category, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
 end
