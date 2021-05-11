8a05138d93b2f78d6a703a5a4f3dc13fce66e6e0
Denis Lafontant <icebox827@gmail.com>
2021-05-11 11:30:29 -0400

    Update book controller

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,2 +1,40 @@
 class BookController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      flash[:notice] = 'Book Add successfully'
+      redirect_to root_path(@book)
+    else
+      flash[:alert] = 'Cannot add book. Please try again'
+      render 'new'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+
+    @book.destroy
+    flash[:alert] = 'The book has been removed'
+    redirect_to root_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :category)
+    redirect_to book_path(@book)
+  end
 end
