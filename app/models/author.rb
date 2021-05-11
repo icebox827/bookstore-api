779daeb24f36e89d87583abe9de58f92fe1f7d49
Denis Lafontant <icebox827@gmail.com>
2021-05-11 18:59:24 -0400

    Add validations to author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ApplicationRecord
-  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :first_name, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
+  validates :last_name, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
 
   has_many :books
 end
